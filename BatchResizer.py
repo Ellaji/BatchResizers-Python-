2ee76f460bec49445a24f9f1224ba3618449c9ef
Ellaji <mirellakersten@gmail.com>
2016-08-09 12:33:07 +0200

    Update BatchResizer.py

--- BatchResizer.py
+++ BatchResizer.py
@@ -1,6 +1,6 @@
-# Part of this code is from: http://pythoncentral.io/resize-image-python-batch/
 # To run this script, PIL or Pillow should be installed
 # Also jpeg and png decoders (libraries) should be installed with PIl or Pillow
+# Part of this code is from: http://pythoncentral.io/resize-image-python-batch/
 
 import os
 # Next is a parser for GETting command line OPTions
